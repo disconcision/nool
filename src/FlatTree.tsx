955778be0cf08766083b513c347bdef09f4d35c3
disconcision <me@andrewblinn.com>
2023-02-06 01:01:43 -0500

    accidentally some files

---
+++ src/FlatTree.tsx
@@ -0,0 +1,11 @@
+export type Id = number;
+export type FlatNode = {t: 'Atom', s: string} | {t: 'Comp', s:string, kids: Id[]};
+export type FlatTree = {root: Id|undefined, nodes: FlatNode[]};
+import {TNode} from './Node';
+
+export let empty: FlatTree = {root:undefined, nodes:[]};
+
+export let update_single = ({root, nodes}: FlatTree, id: Id, f: (_:FlatNode)=>FlatNode): FlatTree => {
+    nodes[id] = f(nodes[id]);
+    return {root, nodes};
+};
