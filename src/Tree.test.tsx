e444afb8c8b45306e26e45726a8ad699b1400d67
disconcision <me@andrewblinn.com>
2023-02-12 17:24:39 -0500

    end-to-end root transform working + unit tests

--- src/Tree.test.tsx
+++ src/Tree.test.tsx
@@ -1,4 +1,5 @@
-import { matches, atom, comp, p_var, p_const, p_comp } from './Tree';
+import { Exp, TransformResult, erase, matches, hydrate, transform, atom, comp, p_var, p_const, p_comp } from './Tree';
+
 
 describe("test matches", () => {
   it("atoms", () => {
@@ -30,3 +31,59 @@ describe("test matches", () => {
     expect(matches(pat_3, tree_3)).toEqual([['b', branch_2], ['c', branch_3]]);
   })
 })
+
+let expect_exp = (e_in:Exp, e_out:Exp)  =>
+  expect(erase(e_in)).toEqual(erase(e_out));
+
+describe("test hydrate", () => {
+  it("basic hydration", () => {
+    expect_exp(hydrate(p_var('a'), []), atom('a'));
+    expect_exp(hydrate(p_const('a'), [['a', atom('1')]]), atom('a'));
+    expect_exp(hydrate(p_var('a'), [['a', atom('1')]]), atom('1'));
+    // duplicate bindings use first for now
+    expect_exp(hydrate(p_var('a'), [['a', atom('1')],['a', atom('2')]]), atom('1'));
+    expect_exp(
+      hydrate(
+        p_comp([p_const('+'), p_var('a'), p_var('b')]),
+        [['a', atom('1')], ['b', atom('2')]]),
+      comp([atom('+'), atom('1'), atom('2')]));
+    expect_exp(
+      hydrate(
+          p_comp([p_const('+'), p_var('a'), p_comp([p_var('b'), p_var('c')])]),
+          [['a', atom('1')], ['c', atom('2')]]),
+        comp([atom('+'), atom('1'), comp([atom('b'), atom('2')])]));   
+  })
+});
+
+describe("test root transform", () => {
+  let expect_exp = (e_in:TransformResult, e_out:TransformResult)  =>
+  expect(((e_in=='NoMatch')?'NoMatch':erase(e_in))).toEqual(((e_out=='NoMatch')?'NoMatch':erase(e_out)));
+  it("transform degenerate cases", () => {
+    expect_exp(transform(atom('1'),p_var('a'), p_var('b')), atom('b'));
+    expect_exp(transform(atom('1'),p_const('1'), p_var('c')), atom('c'));
+    expect_exp(transform(atom('1'),p_const('2'), p_var('d')), 'NoMatch');
+  })
+  
+  it("end to end transform basic", () => {
+    expect_exp(transform(atom('1'),p_var('a'), p_var('a')), atom('1'));
+    expect_exp(transform(atom('1'),p_const('b'), p_var('b')), 'NoMatch');
+  })
+
+  it("end to end commutativity +", () => {
+    expect_exp(
+      transform(
+        comp([atom('+'), atom('1'), atom('2')]),
+        p_comp([p_const('+'), p_var('a'), p_var('b')]),
+        p_comp([p_const('+'), p_var('b'), p_var('a')])),
+      comp([atom('+'), atom('2'), atom('1')]));
+  })
+
+  it("end to end associativity +", () => {
+      expect_exp(
+        transform(
+          comp([atom('+'), atom('1'), comp([atom("+"),atom('2'), atom('3')])]),
+          p_comp([p_const('+'), p_var('a'), p_comp([p_const('+'), p_var('b'), p_var('c')])]),
+          p_comp([p_const('+'), p_comp([p_const('+'), p_var('a'), p_var('b')]), p_var('c')])),
+        comp([atom('+'), comp([atom('+'), atom('1'), atom('2')]), atom('3')]));
+  })
+});
--- src/Tree.tsx
+++ src/Tree.tsx
@@ -15,6 +15,12 @@ export type Exp = Node<string>;
 export const atom = (sym: string): Exp => ({t: 'Atom', id:new_id(), sym});
 export const comp = (kids: Exp[]): Exp => ({t: 'Comp', id:new_id(), kids});
 
+export const erase = (e: Exp): Exp => {
+  switch(e.t) {
+    case 'Atom': return {t: 'Atom', id:0, sym: e.sym};
+    case 'Comp': return {t: 'Comp', id:0, kids: e.kids.map(erase)};}};
+    
+
 export const depth = (node: Exp):number  => {switch(node.t){
   case 'Atom': return 0;
   case 'Comp': return 1 + Math.max(...node.kids.map(depth));
@@ -27,31 +33,55 @@ export const p_comp = (kids: Pat[]): Pat => ({t: 'Comp', id:new_id(), kids});
 
 type Binding = [string, Exp];
 type MatchResult = Binding[] | 'NoMatch';
+export type TransformResult = Exp | 'NoMatch';
 
-export const matches = (pat: Pat, tree: Exp): MatchResult => {
+/* Try to match Exp with Pat at the root of Exp */
+export const matches = (pat: Pat, exp: Exp): MatchResult => {
   switch(pat.t) {
     case 'Atom': {
       switch(pat.sym.t) {
-        case 'Var': return [[pat.sym.name, tree]];
+        case 'Var': return [[pat.sym.name, exp]];
         case 'Const': {
-          switch(tree.t) {
-            case 'Atom': return pat.sym.name == tree.sym ? [] : 'NoMatch';
+          switch(exp.t) {
+            case 'Atom': return pat.sym.name == exp.sym ? [] : 'NoMatch';
             case 'Comp': return 'NoMatch'; }}}};
     break;
     case 'Comp': {
-      switch(tree.t) {
+      switch(exp.t) {
         case 'Atom': return 'NoMatch';
-        case 'Comp': return kidsmatch(pat.kids, tree.kids);
+        case 'Comp': return kidsmatch(pat.kids, exp.kids);
 }}}};
+
+const var_hydrate = (bindings: Binding[],pat_name:string) =>{
+  let binding = bindings.find(([name, _]) => name == pat_name);
+  if (binding) return binding[1];
+  else return atom(pat_name); //TODO: error instead?
+}
+
+/* Recursively substitute the provided bindings into the Pat template */
+export const hydrate = (pat: Pat, bindings: Binding[]): Exp => {
+  switch(pat.t) {
+    case 'Atom': {
+      switch(pat.sym.t) {
+        case 'Var': return var_hydrate(bindings,pat.sym.name);
+        case 'Const': return atom(pat.sym.name); }}
+    case 'Comp': return comp(pat.kids.map(kid => hydrate(kid, bindings)));
+}};
+
+export const transform = (exp: Exp, pat: Pat, template: Pat): TransformResult => {
+  let bindings = matches(pat, exp);
+  if (bindings === 'NoMatch') return 'NoMatch';
+  return hydrate(template, bindings);
+};
 
 let concat_bindings = (a: MatchResult, b: MatchResult): MatchResult => {
     if (a === 'NoMatch' || b === 'NoMatch') return 'NoMatch';
     return a.concat(b);
 };
 
-let kidsmatch = (pats: Pat[], trees: Exp[]): MatchResult => {
-  if (pats.length !== trees.length) return 'NoMatch';
-  return zip(pats, trees)
+let kidsmatch = (pats: Pat[], exps: Exp[]): MatchResult => {
+  if (pats.length !== exps.length) return 'NoMatch';
+  return zip(pats, exps)
     .map(([p, t]) => matches(p, t))
     .reduce(concat_bindings, [])
 };
