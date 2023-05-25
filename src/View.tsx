5eea884a9142cfa76927c3141e4bcd556b0a5667
disconcision <me@andrewblinn.com>
2023-05-24 22:58:51 -0400

    paint.plan

--- src/View.tsx
+++ src/View.tsx
@@ -342,3 +342,37 @@ export const Toolbar: Component<{ model: Model; inject: Inject }> = (props) => {
     </div>
   );
 };
+
+/*
+paint plan:
+
+this version only label nodes; does not replace them
+we have a map from ids to (user-specified) attributes including paint color
+paint colors come from a fixed palette which includes emojicon labels
+
+flavor: Unflavored | {color: string, label: string}
+palette: list(flavor)
+attributes: {
+  flavor,
+  show = Overlay | Collapsed // overlay is color if flavored, collapsed is color + label or "..." if unflavored
+}
+
+model 1:
+  model.paint = map(ids, attributes) 
+
+view 1:
+  for now implictly supress child Overlays, though retained Collapsed
+
+actions 1:
+  paintNode(id, flavor)
+  showNode(id, show)
+  unpaintAllOf(flavor)
+  unpaintAll()
+  
+and then after that works, we can add a second painting abstraction, antipainting:
+  add new flavor 'EscapePaint' (name needs work)
+  this is only meaningful on decendents of a painted node
+  and conceptually it creates a delimited multi-holed context
+  simple use case: quasi-folding everything above a selected node
+
+*/
