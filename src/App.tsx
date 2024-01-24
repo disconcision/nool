091296bc9f92e30937078310134929b629d04720
disconcision <me@andrewblinn.com>
2024-01-24 15:00:18 -0500

    disable view transitions if not supported

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: Component = () => {
     //   console.log("blocked:" + a.t);
     //   return;
     // }
-    if (a.t === "setHover") {
+    if (a.t === "setHover" || ~document.startViewTransition) {
       console.log("sethover dont transition:" + a.t);
       go(model, setModel, a)
       return;
